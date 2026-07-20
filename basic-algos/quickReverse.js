75bd69754f8dcaa975de91b77f099c3746f760ca
Suyash <suyashrusia2005@gmail.com>
2026-07-20 11:25:06 +0530

    feat: Implement foundational algorithms and DS examples
    This commit introduces new educational content, including:
    - Two classic algorithms: QuickSort (descending order) and Tower of Hanoi (a recursive puzzle solver).
    - Basic data structure examples: Demonstrations of fundamental array operations and a placeholder for object concepts.
    Files added to 'basic-algos' and 'data-structures' directories.

---
+++ basic-algos/quickReverse.js
@@ -0,0 +1,26 @@
+const quick = (arr) => {
+    if (arr.length < 2) {
+        return arr;
+    }
+
+    let pivot = arr[arr.length-1];
+    let left = [];
+    let right = [];
+
+    for (let i = 0; i < arr.length-1; i++) {
+        if (arr[i] < pivot) {
+            right.push(arr[i])
+        } else {
+            left.push(arr[i])            
+        }        
+    }
+
+    console.log("Pivot: ", pivot);
+
+
+    return [...quick(left), pivot, ...quick(right)]
+}
+
+let arr = [8, 20, -6, -2, 4];
+
+console.log(quick(arr))
---
+++ data-structures/arrays.js
@@ -0,0 +1,16 @@
+const arr = [1,2,3,'Suyash']
+
+console.log(arr[0])
+arr.push(4)
+console.log(arr[arr.length-1])
+arr.forEach(element => {
+    console.log(item)    
+});
+
+arr.pop()
+arr.unshift(10);
+console.log(arr[0])
+arr.shift()
+console.log(arr[0])
+
+//Map, filter, reduce, concat, slice and splice
